67d2e7a7c5c9157560b9639b22cfa70afcae6733
randy mike <randymark6300@gmail.com>
2020-10-03 16:27:01 +0530

    new

--- queue1.py
+++ queue1.py
@@ -1,2 +1,27 @@
-print('hello')
-print(2**2)
+class Queue:
+    def __init__(self):
+        self.items = deque()
+
+    def is_empty(self):
+        return not self.items
+        # return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.popleft()
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[0]
+
+    def __str__(self):
+        return str(self.items)
+
+
+if __name__ == "__main__":
+    q = Queue()
+
